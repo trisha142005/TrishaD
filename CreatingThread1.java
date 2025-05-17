e4c53a40a2eedfad2fccb9399abe51cff799acfe
trupti142005 <shettytrisha49@gmail.com>
2025-05-17 10:21:12 +0530

    Add files via upload

---
+++ CreatingThread1.java
@@ -0,0 +1,36 @@
+class NewThread implements Runnable{
+Thread t;
+NewThread(){
+t=new Thread(this, "Demo Thread");
+System.out.println("Child thread: "+t);
+t.start();
+}
+public void run(){
+try{
+for(int i=5;i>0;i--){
+System.out.println("Child thread: "+i);
+Thread.sleep(500);
+}
+}
+catch(InterruptedException e){
+System.out.println("Child Interrupted");
+}
+System.out.println("Exiting child thread.");
+}
+}
+
+class CreatingThread1{
+public static void main(String args[]){
+new NewThread();
+try{
+for(int i=5;i>0;i--){
+System.out.println("Main Thread: "+i);
+Thread.sleep(1000);
+}
+}
+catch(InterruptedException e){
+System.out.println("Main Thread interrupted.");
+}
+System.out.println("Main thread exiting.");
+}
+}
